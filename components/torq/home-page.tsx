f2aa00f4fc9b5c99ded4f8d2e9bbd543ae98284c
XHIBT007 <baiohpm007@gmail.com>
2026-08-29 17:43:36 +0100

    Create home-page.tsx

---
+++ components/torq/home-page.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { About } from '@/components/torq/about'
+import { Contact } from '@/components/torq/contact'
+import { Experiences } from '@/components/torq/experiences'
+import { Faq } from '@/components/torq/faq'
+import { Footer } from '@/components/torq/footer'
+import { Gallery } from '@/components/torq/gallery'
+import { Hero } from '@/components/torq/hero'
+import { Navbar } from '@/components/torq/navbar'
+import { RegistrationProvider } from '@/components/torq/registration'
+import { Sponsors } from '@/components/torq/sponsors'
+
+export function HomePage() {
+  return (
+    <RegistrationProvider>
+      <div className="relative min-h-screen bg-background">
+        <Navbar />
+
+        <main>
+          <Hero />
+          <About />
+          <Experiences />
+          <Gallery />
+          <Sponsors />
+          <Faq />
+          <Contact />
+        </main>
+
+        <Footer />
+      </div>
+    </RegistrationProvider>
+  )
+}
