9958df683176f31e67c522936f1217e1f8404a7d
XHIBT007 <baiohpm007@gmail.com>
2026-08-29 17:44:40 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,5 @@
-import { About } from '@/components/torq/about'
-import { Contact } from '@/components/torq/contact'
-import { Experiences } from '@/components/torq/experiences'
-import { Faq } from '@/components/torq/faq'
-import { Footer } from '@/components/torq/footer'
-import { Gallery } from '@/components/torq/gallery'
-import { Hero } from '@/components/torq/hero'
-import { Navbar } from '@/components/torq/navbar'
-import { RegistrationProvider } from '@/components/torq/registration'
-import { Sponsors } from '@/components/torq/sponsors'
+import { HomePage } from '@/components/torq/home-page'
 
 export default function Page() {
-  return (
-    <RegistrationProvider>
-      <div className="relative min-h-screen bg-background">
-        <Navbar />
-        <main>
-          <Hero />
-          <About />
-          <Experiences />
-          <Gallery />
-          <Sponsors />
-          <Faq />
-          <Contact />
-        </main>
-        <Footer />
-      </div>
-    </RegistrationProvider>
-  )
+  return <HomePage />
 }
