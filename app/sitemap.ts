1073641d4e241e19f4891714998be9d63449751e
XHIBT007 <baiohpm007@gmail.com>
2026-09-17 07:50:27 +0100

    Update base URL in sitemap function

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://tor-q-event-website.vercel.app'
+  const baseUrl = 'https://torq.ng'
 
   return [
     {
